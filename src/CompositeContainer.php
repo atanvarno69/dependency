12dd15bc8fee1b85bef47acc93dcb8b8d93bfbc4
atanvarno <atanvarno69@users.noreply.github.com>
2016-11-23 16:13:38 +0000

    Update CompositeContainer.php

--- src/CompositeContainer.php
+++ src/CompositeContainer.php
@@ -55,7 +55,7 @@ use Throwable;
 class CompositeContainer extends Container
 {
     /** Traits use block */
-    use CompositeTrait, ContainerTrait, LoggerAwareTrait;
+    use CompositeTrait;
     
     /**
      * Constructor
