d5039dbc786feceb3ee3bff036bd3586a682c73d
atanvarno <atanvarno69@gmail.com>
2017-03-30 15:11:51 +0100

    Create InvalidArgumentException.php

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package   Atanvarno\Dependency
+ * @author    atanvarno69 <https://github.com/atanvarno69>
+ * @copyright 2017 atanvarno.com
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+ 
+namespace Atanvarno\Dependency\Exception;
+
+/** 
+ * Atanvarno\Dependency\Exception\InvalidArgumentException
+ *
+ * @internal Exception implementing `ContainerExceptionInterface`.
+ */
+class InvalidArgumentException extends ContainerException
+{
+    
+}
