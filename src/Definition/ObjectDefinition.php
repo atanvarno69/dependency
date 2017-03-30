b49c8bdcb375241d537dc5861ac3056ce33788cf
atanvarno <atanvarno69@gmail.com>
2017-03-30 15:09:53 +0100

    Create ObjectDefinition.php

---
+++ src/Definition/ObjectDefinition.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package   Atanvarno\Dependency
+ * @author    atanvarno69 <https://github.com/atanvarno69>
+ * @copyright 2017 atanvarno.com
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+namespace Atanvarno\Dependency\Definition;
+
+class ObjectDefinition extends AbstractDefinition
+{
+    private $className, $parameters;
+    
+    public function __construct(
+        string $className,
+        array $parameters,
+        bool $register
+    ) {
+        if (!class_exists($className) {
+            $msg = sprintf('%s is not a valid class name', $className);
+            throw new ContainerException($msg);
+        }
+        $this->className = $className;
+        $this->parameters = $parameters;
+        parent::__construct($register);
+    }
+    
+    protected function factoryMethod(ContainerInterface $container)
+    {
+        $parameters = $this->resolveParameter($this->parameters);
+        return new {$this->className}(...$parameters);
+    }
+}
