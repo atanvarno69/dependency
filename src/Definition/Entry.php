759da99fca238606886a00ab8d305e95a9c2d512
atanvarno <atanvarno69@gmail.com>
2017-03-30 15:07:08 +0100

    Create Entry.php

---
+++ src/Definition/Entry.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package   Atanvarno\Dependency
+ * @author    atanvarno69 <https://github.com/atanvarno69>
+ * @copyright 2017 atanvarno.com
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+namespace Atanvarno\Dependency\Definition;
+
+/**
+ * Atanvarno\Dependency\Definition\Entry
+ *
+ * @internal For use as a proxy parameter for a container entry.
+ */
+class Entry
+{
+    /** @var string $id Identifier of the proxy entry. */
+    private $id;
+    
+    /**
+     * Builds an `Entry` instance.
+     *
+     * @param string $id Identifier of the proxy entry.
+     */
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+    
+    /**
+     * @return string Identifier of the proxy entry.
+     */
+    public function __toString(): string
+    {
+        return $this->id;
+    }
+}
