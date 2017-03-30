8e364fecad82b6d14b530d0ddbad765c36dd0487
atanvarno <atanvarno69@gmail.com>
2017-03-30 15:09:21 +0100

    Create FactoryDefinition.php

---
+++ src/Definition/FactoryDefinition.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package   Atanvarno\Dependency
+ * @author    atanvarno69 <https://github.com/atanvarno69>
+ * @copyright 2017 atanvarno.com
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+namespace Atanvarno\Dependency\Definition;
+
+class FactoryDefinition extends AbstractDefinition
+{
+    private $callable, $parameters;
+    
+    public function __construct(
+        callable $callable,
+        array $parameters,
+        bool $register
+    ) {
+        $this->callable = $callable;
+        $this->parameters = $parameters;
+        parent::__construct($register);
+    }
+    
+    protected function factoryMethod(ContainerInterface $container)
+    {
+        $parameters = $this->resolveParameter($this->parameters);
+        return call_user_func($this->callable, ...$parameters);
+    }
+}
