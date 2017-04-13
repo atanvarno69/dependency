22ce08bae52640f920015186de5f1f71add536b7
atanvarno <atanvarno69@gmail.com>
2017-04-13 10:00:06 +0100

    Create InvalidArgumentExceptionTest.php

---
+++ tests/InvalidArgumentExceptionTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package   Atanvarno\Dependency
+ * @author    atanvarno69 <https://github.com/atanvarno69>
+ * @copyright 2017 atanvarno.com
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+namespace Atanvarno\Dependency\Test;
+
+/** SPL use block. */
+use Exception;
+
+/** PHPUnit use block. */
+use PHPUnit\Framework\TestCase;
+
+/** Package use block. */
+use Atanvarno\Dependency\Exception\{
+    ContainerException, InvalidArgumentException
+};
+
+class NotFoundExceptionTest extends TestCase
+{   
+    public function testContainerExceptionImplementsInterface()
+    {
+        $exception = new InvalidArgumentException();
+        $this->assertInstanceOf(Exception::class, $exception);
+        $this->assertInstanceOf(ContainerException::class, $exception);
+    }
+}
