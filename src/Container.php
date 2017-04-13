6ce479fcfa6bb2d0c7079cf5a90f0c880b3da436
atanvarno <atanvarno69@gmail.com>
2017-04-13 10:30:08 +0100

    Update Container.php

--- src/Container.php
+++ src/Container.php
@@ -290,7 +290,7 @@ class Container implements ArrayAccess, ContainerInterface
             );
         }
         if (!is_array($return) {
-            $type = (is_object($return))
+            $type = (is_object($return)
                 ? get_class($return)
                 : get_type($return);
             $msg = sprintf(
